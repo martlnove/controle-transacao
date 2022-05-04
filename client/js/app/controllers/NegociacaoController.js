2be1068c2b623840a16b51cadc7d10bc6ebcc3ac
Lucas Martins <lucasshc@gmail.com>
2022-05-04 11:40:35 -0300

    Ajuste no construtor do Date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,12 +8,15 @@ class NegociacaoController {
     }
     adiciona(event){
         event.preventDefault();
-        
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice)=> item - indice % 2)
+        );
         let negociacao = new Negociacao(
-            this._inputData.value,
+            data,
             this._inputQuantidade.value,
             this._inputValor
-        );
-        
-        }
+        )
+    }
 }
