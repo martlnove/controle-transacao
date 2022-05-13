a207fafa724b647d2c923be2b6184cc28dcbcd1b
Lucas Martins <lucasshc@gmail.com>
2022-05-13 13:19:10 -0300

    Validação da data usando expressão regular

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,7 +5,8 @@ class DateHelper{
     }
 
     dataParaTexto(data){
-        /\d{4}-\d{2}-\d{2}/
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formato AAAA-MM-DD');
+
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()} `;
     }
 
