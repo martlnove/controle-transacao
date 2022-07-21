bb36dc4435863d6b0418b09cfe4357ae959b47e2
Lucas Martins <lucasshc@gmail.com>
2022-07-21 15:58:34 -0300

    Update proxy factory

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,22 +1,17 @@
 class ListaNegociacoes {
-    
-    constructor() {
-        
-        this._negociacoes = [];
-    }
-    
-    adiciona(negociacao) {
-        
-        this._negociacoes = [].concat(this._negociacoes, negociacao);
+  constructor() {
+    this._negociacoes = [];
+  }
 
-    }
-    
-    get negociacoes() {
-        
-        return [].concat(this._negociacoes);
-    }
+  adiciona(negociacao) {
+    this._negociacoes = [].concat(this._negociacoes, negociacao);
+  }
 
-    esvazia(){
-        this._negociacoes = [];
-    }
+  get negociacoes() {
+    return [].concat(this._negociacoes);
+  }
+
+  esvazia() {
+    this._negociacoes = [];
+  }
 }
